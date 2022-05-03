d97159f3cbe15d4c4d327887eb3e58d3c255db89
jwaes <jaco.waes@gmail.com>
2022-05-03 11:24:03 +0000

    bump version number

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Manufacturing',
-    'version': '15.0.1.0.8',
+    'version': '15.0.1.0.9',
 
     # any module necessary for this one to work correctly
     'depends': ['mrp','mrp_subcontracting','stock','project'],
