1eff567761b2a3efee70af0e9eae2786c5b79e7f
jwaes <jaco.waes@gmail.com>
2022-11-30 12:20:05 +0000

    floating points

--- models/mrp_bom.py
+++ models/mrp_bom.py
@@ -1,5 +1,5 @@
 import logging
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 _logger = logging.getLogger(__name__)
 
@@ -38,7 +38,7 @@ class MrpBom(models.Model):
                         pricelist = product.otf_bom_template.pricelist_id
                         sale_price = pricelist.get_product_price(line_product, qty, None)
 
-                        sale_line_price = qty * sale_price
+                        sale_line_price = round(qty * sale_price, 2)
 
                         sale_log_lines += "<tr><td>[{}]</td><td>{:.2f}</td><td>{}</td><td>{:.2f}</td><td>{:.2f}</td></tr>".format(line_product.default_code, qty, bom_line.product_uom_id.name,  sale_price, sale_line_price)
                         _logger.info("Sale - {:.2f} * {:.2f} = {:.2f}".format(qty, sale_price, sale_line_price))
@@ -54,7 +54,8 @@ class MrpBom(models.Model):
 
                             if product.otf_bom_template.subcontractor.id == seller.name.id:
                                 purchase_price = seller.price
-                                purchase_line_price = qty * purchase_price
+
+                                purchase_line_price = round(qty * purchase_price, 2)
 
                                 purchase_log_lines += "<tr><td>[{}]</td><td>{:.2f}</td><td>{}</td><td>{:.2f}</td><td>{:.2f}</td></tr>".format(line_product.default_code, qty, bom_line.product_uom_id.name, purchase_price, purchase_line_price)
                                 _logger.info("Purchase - {:.2f} * {:.2f} = {:.2f}".format(qty, purchase_price, purchase_line_price))
@@ -71,7 +72,7 @@ class MrpBom(models.Model):
                 if product.otf_bom_list_price:
                     _logger.info('Updating sale price ?')                
                     product.list_price = new_list_price
-                    if old_list_price != new_list_price:
+                    if tools.float_compare(old_list_price, new_list_price, precision_digits=2):
                         _logger.info("Updating ... changed [{}] to [{}]".format(old_list_price, new_list_price))
                         body += '<h6>Sales</h6><p>Sale price has changed from {:.2f} to {:.2f}.</p>'.format(
                             old_list_price, new_list_price)
@@ -86,7 +87,8 @@ class MrpBom(models.Model):
                     for seller in product.seller_ids:
                         # product.seller_ids.price = product_purchase_price
                         seller.price = new_purchase_price
-                        if old_purchase_price != new_purchase_price:
+                        if tools.float_compare(old_purchase_price, new_purchase_price, precision_digits=2):
+                            _logger.info("Updating ... changed [{}] to [{}]".format(old_purchase_price, new_purchase_price))
                             body += '<h6>Purchase</h6><p>Purchase price has changed from {:.2f} to {:.2f}.</p>'.format(
                                 old_purchase_price, new_purchase_price)
                             body += '<table class="table"><thead><tr><th>Product</th><th>Qty</th><th>UoM</th><th>Price</th><th>Total</th></tr></thead><tbody>' + purchase_log_lines + '<tr><td colspan="4"></td><td>{:.2f}</td></tr></tbody></table>'.format(new_purchase_price)
