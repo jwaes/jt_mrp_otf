a1a3f4b486eb7af27393e95538326c4b8f0e7f84
jwaes <jaco.waes@gmail.com>
2024-06-19 07:59:11 +0000

    f

--- __manifest__.py
+++ __manifest__.py
@@ -17,14 +17,18 @@
     'version': '2.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['mrp','mrp_subcontracting','stock','project','jt_sale_order_line_codecolumn'],
+    'depends': ['mrp',
+                'mrp_subcontracting',
+                'stock',
+                'project',
+                # 'jt_sale_order_line_codecolumn',
+                ],
 
     # always loaded
     'data': [
         'report/purchase_order_templates.xml',
         'report/sale_report_templates.xml',
         'security/ir.model.access.csv',
-        'views/mrp_bom_views.xml',
         'views/otf_bom.xml',
         'views/product_views.xml',
         'views/project_views.xml',
