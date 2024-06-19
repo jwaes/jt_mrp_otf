24548414081e715577159af0c76ce0968f72fa68
jwaes <jaco.waes@gmail.com>
2024-06-19 08:41:17 +0000

    text-right

--- models/mrp_bom.py
+++ models/mrp_bom.py
@@ -40,7 +40,7 @@ class MrpBom(models.Model):
 
                         sale_line_price = round(qty * sale_price, 2)
 
-                        sale_log_lines += "<tr><td>[{}]</td><td>{:.2f} {}</td><td class='text-right'>{:.2f}</td><td class='text-right'>{:.2f}</td></tr>".format(line_product.default_code, qty, bom_line.product_uom_id.name,  sale_price, sale_line_price)
+                        sale_log_lines += "<tr><td>[{}]</td><td>{:.2f} {}</td><td class='text-end'>{:.2f}</td><td class='text-end'>{:.2f}</td></tr>".format(line_product.default_code, qty, bom_line.product_uom_id.name,  sale_price, sale_line_price)
                         _logger.info("Sale - {:.2f} * {:.2f} = {:.2f}".format(qty, sale_price, sale_line_price))
 
                         new_list_price += sale_line_price
@@ -57,7 +57,7 @@ class MrpBom(models.Model):
 
                                 purchase_line_price = round(qty * purchase_price, 2)
 
-                                purchase_log_lines += "<tr><td>[{}]</td><td>{:.2f} {}</td><td class='text-right'>{:.2f}</td><td class='text-right'>{:.2f}</td></tr>".format(line_product.default_code, qty, bom_line.product_uom_id.name, purchase_price, purchase_line_price)
+                                purchase_log_lines += "<tr><td>[{}]</td><td>{:.2f} {}</td><td class='text-end'>{:.2f}</td><td class='text-end'>{:.2f}</td></tr>".format(line_product.default_code, qty, bom_line.product_uom_id.name, purchase_price, purchase_line_price)
                                 _logger.info("Purchase - {:.2f} * {:.2f} = {:.2f}".format(qty, purchase_price, purchase_line_price))
 
                                 new_purchase_price += purchase_line_price                                    
@@ -76,7 +76,7 @@ class MrpBom(models.Model):
                         _logger.info("Updating ... changed [{}] to [{}]".format(old_list_price, new_list_price))
                         body += '<h6>Sales</h6><p>Sale price has changed from {:.2f} to {:.2f}</p>'.format(
                             old_list_price, new_list_price)
-                        body += '<table class="table"><thead><tr><th>Product</th><th>Qty</th><th class="text-right">Price</th><th class="text-right">Total</th></tr></thead><tbody>' + sale_log_lines + '<tr><td colspan="3"></td><td class="text-right">{:.2f}</td></tr></tbody></table>'.format(new_list_price)
+                        body += '<table class="table"><thead><tr><th>Product</th><th>Qty</th><th class="text-end">Price</th><th class="text-end">Total</th></tr></thead><tbody>' + sale_log_lines + '<tr><td colspan="3"></td><td class="text-end">{:.2f}</td></tr></tbody></table>'.format(new_list_price)
                     else :
                         _logger.info('Nothing to update')
                 else :
@@ -91,7 +91,7 @@ class MrpBom(models.Model):
                             _logger.info("Updating ... changed [{}] to [{}]".format(old_purchase_price, new_purchase_price))
                             body += '<h6>Purchase</h6><p>Purchase price has changed from {:.2f} to {:.2f}</p>'.format(
                                 old_purchase_price, new_purchase_price)
-                            body += '<table class="table"><thead><tr><th>Product</th><th>Qty</th><th class="text-right">Price</th><th class="text-right">Total</th></tr></thead><tbody>' + purchase_log_lines + '<tr><td colspan="3"></td><td class="text-right">{:.2f}</td></tr></tbody></table>'.format(new_purchase_price)
+                            body += '<table class="table"><thead><tr><th>Product</th><th>Qty</th><th class="text-end">Price</th><th class="text-end">Total</th></tr></thead><tbody>' + purchase_log_lines + '<tr><td colspan="3"></td><td class="text-end">{:.2f}</td></tr></tbody></table>'.format(new_purchase_price)
                         else :
                             _logger.info('Nothing to update')                                
                 else :
